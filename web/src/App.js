5248d305710b00c99d69f788ee51f9965772576d
Lucas Matheus <lucasag0408@outlook.com>
2020-07-07 19:26:31 -0400

    Adicionando arquivos da aplicação web

---
+++ web/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
+
+function App() {
+    const[devs, setDevs] = useState([]);
+  
+   
+    useEffect(() => {
+        async function reloadDevs(){
+           const response = await api.get('/devs');
+           
+           setDevs(response.data);
+       }  
+
+       reloadDevs();
+
+    }, []);
+    
+    //Função que adiciona um novo dev à base da dados
+    async function handleAddDev(data){
+        const response = await api.post("/devs", data);
+        
+        setDevs([...devs, response.data]);
+    }
+  
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev}/>
+      </aside>
+
+      <main>
+        <ul>
+              {devs.map(dev => (
+                  <DevItem key={dev._id} dev={dev} />
+              ))}
+        </ul>
+      </main>
+
+    </div>
+  );
+} 
+
+export default App;
---
+++ web/src/components/DevForm/index.js
@@ -0,0 +1,103 @@
+import React, {useState, useEffect} from "react"
+import './styles.css'
+
+function DevForm({onSubmit}){
+
+    const[github_username, setGithunusername] = useState('');
+    const[techs, setTechs] = useState('');
+    const[latitude, setLatitude] = useState('');
+    const[longitude, setLongitude] = useState('');
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const {latitude, longitude} = position.coords
+
+                setLatitude(latitude)
+                setLongitude(longitude)
+
+                console.log(latitude, longitude);
+
+            },
+            err => {
+                console.log(err)
+            },
+            {
+                timeout: 30000,
+            }
+        )
+    }, []);
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        await onSubmit({
+            github_username,
+            techs,
+            latitude,
+            longitude,
+        });
+
+        setGithunusername('');
+        setTechs('');
+    
+    }
+    
+    return(
+
+        <form onSubmit={handleSubmit}>
+           <div className="input-block">
+             <label htmlFor="github_username">Usuário do Git-Hub</label>
+             <input 
+              name="github_username" 
+              id="github_username" 
+              required 
+              value={github_username}
+              onChange={e => setGithunusername(e.target.value)}
+             />              
+           </div>
+
+           <div className="input-block">
+              <label htmlFor="techs">Tecnologias</label>
+              <input 
+              name="techs" 
+              id="techs" 
+              required 
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+              />
+           </div>
+
+           <div className="input-group">
+              <div className="input-block">
+                  <label htmlFor="latitude">latitude</label>
+                  <input 
+                  type="number"
+                  required 
+                  name="latitude" 
+                  id="latitude" 
+                  value={latitude}
+                  onChange={e => setLatitude(e.target.value)} 
+                   />
+              </div>
+
+              <div className="input-block">
+                  <label htmlFor="longitude">longitude</label>
+                  <input 
+                  type="number"
+                  required 
+                  name="longitude" 
+                  id="longitude" 
+                  value={longitude} 
+                  onChange={e => setLongitude(e.target.value)} 
+                   />
+              </div>
+
+           </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+    );
+}
+
+export default DevForm
